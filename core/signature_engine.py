c13e02ed2c8c72f1f7c328209cd71c48f2726159
kidus mulugeta <kmulugeta036@gmail.com>
2026-05-10 16:27:50 +0300

    signature function added

--- core/signature_engine.py
+++ core/signature_engine.py
@@ -5,3 +5,10 @@ SIGNATURES = {
     "ZIP": b"PK\x03\x04",
     "EXE": b"MZ"
 }
+def detect_file_type(header):
+    for file_type, signature in SIGNATURES.items():
+
+        if header.startswith(signature):
+            return file_type
+
+    return "Unknown"
