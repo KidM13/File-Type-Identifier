b9c6e5578a1f5d3c580ea2f02b45c1d63448cd37
kidus mulugeta <kmulugeta036@gmail.com>
2026-05-14 12:24:59 +0300

    extension map is added

--- core/signature_engine.py
+++ core/signature_engine.py
@@ -5,6 +5,15 @@ SIGNATURES = {
     "ZIP": b"PK\x03\x04",
     "EXE": b"MZ"
 }
+
+EXTENSION_MAP = {
+    "png": "PNG",
+    "jpg": "JPG",
+    "jpeg": "JPG",
+    "pdf": "PDF",
+    "zip": "ZIP",
+    "exe": "EXE"
+}
 def detect_file_type(header):
     for file_type, signature in SIGNATURES.items():
 
