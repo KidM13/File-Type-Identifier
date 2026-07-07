feb900961411b7c96447d8b3f4dcaa41901475f0
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-07-07 15:08:01 +0300

    adding a function that convert hex to bytes

--- core/signature_engine.py
+++ core/signature_engine.py
@@ -2,3 +2,6 @@ import json
 def load_signature():
     with open("signatures/signatures.json", "r") as f:
         return json.load(f)
+
+def convert_hex_to_bytes(hex_string):
+    return bytes.fromhex(hex_string)
